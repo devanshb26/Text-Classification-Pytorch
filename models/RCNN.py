c8e51a8a52f0081e1c39a39a0f9065c1680b9723
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-15 23:57:11 +0530

    Update RCNN.py

--- models/RCNN.py
+++ models/RCNN.py
@@ -6,7 +6,7 @@ from torch.autograd import Variable
 from torch.nn import functional as F
 
 class RCNN(nn.Module):
-	def __init__(self, batch_size, output_size, hidden_size, vocab_size, embedding_length, weights):
+	def __init__(self, batch_size, output_size, hidden_size, vocab_size, embedding_length, weights,n_layers,dropout):
 		super(RCNN, self).__init__()
 		
 		"""
@@ -30,6 +30,7 @@ class RCNN(nn.Module):
 		self.word_embeddings = nn.Embedding(vocab_size, embedding_length)# Initializing the look-up table.
 		self.word_embeddings.weight = nn.Parameter(weights, requires_grad=False) # Assigning the look-up table to the pre-trained GloVe word embedding.
 		self.dropout = 0.8
+		
 		self.lstm = nn.LSTM(embedding_length, hidden_size, dropout=self.dropout, bidirectional=True)
 		self.W2 = nn.Linear(2*hidden_size+embedding_length, hidden_size)
 		self.label = nn.Linear(hidden_size, output_size)
@@ -61,14 +62,14 @@ class RCNN(nn.Module):
 		"""
 		input = self.word_embeddings(input_sentence) # embedded input of shape = (batch_size, num_sequences, embedding_length)
 		input = input.permute(1, 0, 2) # input.size() = (num_sequences, batch_size, embedding_length)
-		if batch_size is None:
-			h_0 = Variable(torch.zeros(2, self.batch_size, self.hidden_size).cuda()) # Initial hidden state of the LSTM
-			c_0 = Variable(torch.zeros(2, self.batch_size, self.hidden_size).cuda()) # Initial cell state of the LSTM
-		else:
-			h_0 = Variable(torch.zeros(2, batch_size, self.hidden_size).cuda())
-			c_0 = Variable(torch.zeros(2, batch_size, self.hidden_size).cuda())
+# 		if batch_size is None:
+# 			h_0 = Variable(torch.zeros(2, self.batch_size, self.hidden_size).cuda()) # Initial hidden state of the LSTM
+# 			c_0 = Variable(torch.zeros(2, self.batch_size, self.hidden_size).cuda()) # Initial cell state of the LSTM
+# 		else:
+# 			h_0 = Variable(torch.zeros(2, batch_size, self.hidden_size).cuda())
+# 			c_0 = Variable(torch.zeros(2, batch_size, self.hidden_size).cuda())
 
-		output, (final_hidden_state, final_cell_state) = self.lstm(input, (h_0, c_0))
+		output, (final_hidden_state, final_cell_state) = self.lstm(input)
 		
 		final_encoding = torch.cat((output, input), 2).permute(1, 0, 2)
 		y = self.W2(final_encoding) # y.size() = (batch_size, num_sequences, hidden_size)
