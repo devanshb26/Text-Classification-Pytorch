34c63d1cf4f482b8b2a4f2b8d98542ad978f3343
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-15 16:39:25 +0530

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -10,9 +10,70 @@ from torchtext import datasets
 from torchtext.vocab import Vectors, GloVe
 import re
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+URL_REGEX = "(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+"
+EMAIL_REGEX = "[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*"
+USER_REGEX = "\\@\\w+"
+
+INVISIBLE_REGEX = '[\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\xff]'
+QUOTATION_REGEX = "[”“❝„\"]+"
+APOSTROPHE_REGEX = "[‘´’̇]+"
+
+PRICE_REGEX = '([\$£€¥][0-9]+|[0-9]+[\$£€¥])'
+DATE_REGEX = '(?:(?:(?:(?:(?<!:)\\b\\\'?\\d{1,4},? ?)?\\b(?:[Jj]an(?:uary)?|[Ff]eb(?:ruary)?|[Mm]ar(?:ch)?|[Aa]pr(?:il)?|May|[Jj]un(?:e)?|[Jj]ul(?:y)?|[Aa]ug(?:ust)?|[Ss]ept?(?:ember)?|[Oo]ct(' \
+             '?:ober)?|[Nn]ov(?:ember)?|[Dd]ec(?:ember)?)\\b(?:(?:,? ?\\\'?)?\\d{1,4}(?:st|nd|rd|n?th)?\\b(?:[,\\/]? ?\\\'?\\d{2,4}[a-zA-Z]*)?(?: ?- ?\\d{2,4}[a-zA-Z]*)?(?!:\\d{1,' \
+             '4})\\b))|(?:(?:(?<!:)\\b\\\'?\\d{1,4},? ?)\\b(?:[Jj]an(?:uary)?|[Ff]eb(?:ruary)?|[Mm]ar(?:ch)?|[Aa]pr(?:il)?|May|[Jj]un(?:e)?|[Jj]ul(?:y)?|[Aa]ug(?:ust)?|[Ss]ept?(?:ember)?|[Oo]ct(' \
+             '?:ober)?|[Nn]ov(?:ember)?|[Dd]ec(?:ember)?)\\b(?:(?:,? ?\\\'?)?\\d{1,4}(?:st|nd|rd|n?th)?\\b(?:[,\\/]? ?\\\'?\\d{2,4}[a-zA-Z]*)?(?: ?- ?\\d{2,4}[a-zA-Z]*)?(?!:\\d{1,4})\\b)?))|(?:\\b(' \
+             '?<!\\d\\.)(?:(?:(?:[0123]?[0-9][\\.\\-\\/])?[0123]?[0-9][\\.\\-\\/][12][0-9]{3})|(?:[0123]?[0-9][\\.\\-\\/][0123]?[0-9][\\.\\-\\/][12]?[0-9]{2,3}))(?!\\.\\d)\\b))'
+TIME_REGEX = '(?:(?:\d+)?\.?\d+(?:AM|PM|am|pm|a\.m\.|p\.m\.))|(?:(?:[0-2]?[0-9]|[2][0-3]):(?:[0-5][0-9])(?::(?:[0-5][0-9]))?(?: ?(?:AM|PM|am|pm|a\.m\.|p\.m\.))?)'
+
 import spacy
 nlp = spacy.load('en')
 def tokenize_en(text):
+  
+  
+  text = re.sub('[\u0370-\u03ff]', '', text)  # Greek and Coptic
+  text = re.sub('[\u0400-\u052f]', '', text)  # Cyrillic and Cyrillic Supplementary
+  text = re.sub('[\u2500-\u257f]', '', text)  # Box Drawing
+  text = re.sub('[\u2e80-\u4dff]', '', text)  # from CJK Radicals Supplement
+  text = re.sub('[\u4e00-\u9fff]', '', text)  # CJK Unified Ideographs
+  text = re.sub('[\ue000-\uf8ff]', '', text)  # Private Use Area
+  text = re.sub('[\uff00-\uffef]', '', text)  # Halfwidth and Fullwidth Forms
+  text = re.sub('[\ufe30-\ufe4f]', '', text)  # CJK Compatibility Forms
+
+  text = re.sub(INVISIBLE_REGEX, '', text)
+  text = re.sub(QUOTATION_REGEX, '\"', text)
+  text = re.sub(APOSTROPHE_REGEX, '\'', text)
+  text = re.sub(r"\s+", " ", text)
+  
+  text = re.sub(PRICE_REGEX, r" <PRICE> ", text)
+  text = re.sub(TIME_REGEX, r" <TIME> ", text)
+  text = re.sub(DATE_REGEX, r" <DATE> ", text)
+
+  text = re.sub(r"([a-zA-Z]+)([0-9]+)", r"\1 \2", text)
+  text = re.sub(r"([0-9]+)([a-zA-Z]+)", r"\1 \2", text)
+  text = re.sub(r" [0-9]+ ", r" <NUMBER> ", text)
+
+  text = re.sub(r"(\b)([Ii]) 'm", r"\1\2 am", text)
+  text = re.sub(r"(\b)([Tt]hey|[Ww]e|[Ww]hat|[Ww]ho|[Yy]ou) 're", r"\1\2 are", text)
+  text = re.sub(r"(\b)([Ll]et) 's", r"\1\2 us", text)
+  text = re.sub(r"(\b)([Hh]e|[Ii]|[Ss]he|[Tt]hey|[Ww]e|[Ww]hat|[Ww]ho|[Yy]ou) 'll", r"\1\2 will", text)
+  text = re.sub(r"(\b)([Ii]|[Ss]hould|[Tt]hey|[Ww]e|[Ww]hat|[Ww]ho|[Ww]ould|[Yy]ou) 've", r"\1\2 have", text)
+  
+  text = re.sub(r"(\b)([Aa]re|[Cc]ould|[Dd]id|[Dd]oes|[Dd]o|[Hh]ad|[Hh]as|[Hh]ave|[Ii]s|[Mm]ight|[Mm]ust|[Ss]hould|[Ww]ere|[Ww]as|[Ww]ould) n't", r"\1\2 not", text)
+  text = re.sub(r"(\b)([Cc]a) n't", r"\1\2n not", text)
+  text = re.sub(r"(\b)([Ww]) on't", r"\1\2ill not", text)
+  text = re.sub(r"(\b)([Ss])han't", r"\1\2hall not", text)
+  text = re.sub(r" n't ", r" not ", text)
+
+  
+  text = re.sub(r"([‼.,;:?!…])+", r" \1 ", text)
+  text = re.sub(r"([()])+", r" \1 ", text)
+  text = re.sub(r"[-]+", r" - ", text)
+  text = re.sub(r"[_]+", r" _ ", text)
+  text = re.sub(r"[=]+", r" = ", text)
+  text = re.sub(r"[\&]+", r" \& ", text)
+  text = re.sub(r"[\+]+", r" \+ ", text)
+
   text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
   text = re.sub(r"what's", "what is", text)
   text = re.sub(r"\'s", "", text)
@@ -65,7 +126,7 @@ def load_dataset(test_sen=None):
     """
     
 #     tokenize = lambda x: x.split()
-    TEXT = data.Field(sequential=True, tokenize='spacy', lower=True, include_lengths=True, batch_first=True, fix_length=40)
+    TEXT = data.Field(sequential=True, tokenize=tokenize_en, lower=True, include_lengths=True, batch_first=True, fix_length=40)
     LABEL = data.LabelField(dtype=torch.float)
     fields = [(None,None),(None,None),('text', TEXT),('label', LABEL)]
     train_data, valid_data, test_data = data.TabularDataset.splits(
@@ -82,7 +143,7 @@ def load_dataset(test_sen=None):
                                         skip_header = True
 ) 
     print(vars(train_data[0]))
-    TEXT.build_vocab(train_data, vectors=GloVe(name='6B', dim=300))
+    TEXT.build_vocab(train_data, vectors=GloVe(name='6B', dim=100))
     LABEL.build_vocab(train_data)
 
     word_embeddings = TEXT.vocab.vectors
