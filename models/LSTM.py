3d789c810eb4325d6311e489810cfda61bbc4b7c
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-16 01:39:54 +0530

    Update LSTM.py

--- models/LSTM.py
+++ models/LSTM.py
@@ -33,8 +33,8 @@ class LSTMClassifier(nn.Module):
 		self.word_embeddings.weight = nn.Parameter(weights, requires_grad=False) # Assigning the look-up table to the pre-trained GloVe word embedding.
 		self.lstm = nn.LSTM(embedding_length, hidden_size,dropout=dropout)
 		self.fc1 = nn.Linear(hidden_size,150)
- 		self.fc2 = nn.Linear(150,25)
-		self.label = nn.Linear(25,output_size)
+		self.fc2 = nn.Linear(150,25)
+ 		self.label = nn.Linear(25,output_size)
 		
 	def forward(self, input_sentence, batch_size=None):
 	
