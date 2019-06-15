681cf1b80a26166fa5eedd469f536c800db99e8f
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-16 00:10:12 +0530

    Update RCNN.py

--- models/RCNN.py
+++ models/RCNN.py
@@ -29,8 +29,8 @@ class RCNN(nn.Module):
 		
 		self.word_embeddings = nn.Embedding(vocab_size, embedding_length)# Initializing the look-up table.
 		self.word_embeddings.weight = nn.Parameter(weights, requires_grad=False) # Assigning the look-up table to the pre-trained GloVe word embedding.
-		self.dropout = 0.8
-		
+		self.dropout = 0.5
+		self.dropout_2 = 0.2
 		self.lstm = nn.LSTM(embedding_length, hidden_size, dropout=self.dropout, bidirectional=True)
 		self.W2 = nn.Linear(2*hidden_size+embedding_length, hidden_size)
 		self.label = nn.Linear(hidden_size, output_size)
@@ -62,7 +62,8 @@ class RCNN(nn.Module):
 		"""
 		input = self.word_embeddings(input_sentence) # embedded input of shape = (batch_size, num_sequences, embedding_length)
 		input = input.permute(1, 0, 2) # input.size() = (num_sequences, batch_size, embedding_length)
-# 		if batch_size is None:
+		input=self.dropout(input)
+#                 if batch_size is None:
 # 			h_0 = Variable(torch.zeros(2, self.batch_size, self.hidden_size).cuda()) # Initial hidden state of the LSTM
 # 			c_0 = Variable(torch.zeros(2, self.batch_size, self.hidden_size).cuda()) # Initial cell state of the LSTM
 # 		else:
@@ -74,6 +75,7 @@ class RCNN(nn.Module):
 		final_encoding = torch.cat((output, input), 2).permute(1, 0, 2)
 		y = self.W2(final_encoding) # y.size() = (batch_size, num_sequences, hidden_size)
 		y = y.permute(0, 2, 1) # y.size() = (batch_size, hidden_size, num_sequences)
+		y=self.droput_2(y)
 		y = F.max_pool1d(y, y.size()[2]) # y.size() = (batch_size, hidden_size, 1)
 		y = y.squeeze(2)
 		logits = self.label(y)
