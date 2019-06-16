b9866086499af6dff581662c3548c4629b479667
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-17 01:03:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -100,10 +100,10 @@ out_channels=250
 kernel_heights=[2,3,4]
 stride=1
 padding=0
-keep_probab=0.3
+keep_probab=0.2
 
 
-model = AttentionModel(batch_size, output_size, hidden_size, vocab_size, embedding_length, word_embeddings,N_LAYERS,DROPOUT)
+model = CNN(batch_size, output_size, hidden_size, vocab_size, embedding_length, word_embeddings,N_LAYERS,DROPOUT)
 # model = CNN(batch_size, output_size, in_channels, out_channels, kernel_heights, stride, padding, keep_probab, vocab_size, embedding_length, word_embeddings)
 # loss_fn = F.cross_entropy
 
