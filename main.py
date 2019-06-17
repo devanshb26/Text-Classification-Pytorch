e7ed96e48396829098b825dc04d393591e0a8d52
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-17 11:14:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,14 +27,16 @@ import random
 import re
 from torch.backends import cudnn
 SEED = 1234
-print(SEED)
-random.seed(SEED)
-np.random.seed(SEED)
-torch.manual_seed(SEED)
+
+seed=random.randint(0, 10000)
+print(seed)
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
 # seed = 0
 # torch.manual_seed(seed)
 if torch.cuda.is_available():
-  torch.cuda.manual_seed_all(SEED)
+  torch.cuda.manual_seed_all(seed)
 torch.backends.cudnn.enabled = False 
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
