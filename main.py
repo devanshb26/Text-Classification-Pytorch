53df9fb31c4ffaeb69728f019e134b56a149c2ad
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-17 13:11:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ output_size = 1
 hidden_size = 256
 N_LAYERS = 2
 #changed from 0.2 to 0.4
-DROPOUT = 0.4
+DROPOUT = 0.2
 embedding_length = 100
 in_channels=1
 out_channels=192
@@ -106,8 +106,8 @@ padding=0
 keep_probab=0.4
 
 
-# model = AttentionModel(batch_size, output_size, hidden_size, vocab_size, embedding_length, word_embeddings,N_LAYERS,DROPOUT)
-model = CNN(batch_size, output_size, in_channels, out_channels, kernel_heights, stride, padding, keep_probab, vocab_size, embedding_length, word_embeddings)
+model = AttentionModel(batch_size, output_size, hidden_size, vocab_size, embedding_length, word_embeddings,N_LAYERS,DROPOUT)
+# model = CNN(batch_size, output_size, in_channels, out_channels, kernel_heights, stride, padding, keep_probab, vocab_size, embedding_length, word_embeddings)
 # loss_fn = F.cross_entropy
 
 import torch.optim as optim
