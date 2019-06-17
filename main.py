78bffc7791d5c01aea3e08a539b1789eb68ce230
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-17 22:25:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -440,7 +440,7 @@ def predict_sentiment(model):
       prediction = torch.sigmoid(model(tensor))
 #       print(prediction)
       print(prediction.size())
-      l.append((prediction[-1][0].data)).cpu().numpy())
+      l.append((prediction[-1][0].data).cpu().numpy())
     df['preds']=l
     import csv
     df.to_csv('predidctions.csv')
