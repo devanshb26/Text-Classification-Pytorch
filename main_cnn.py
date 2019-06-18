bab7cd61983c9236b534e581fcde412e4b54a23f
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-18 12:14:08 +0530

    Update main_cnn.py

--- main_cnn.py
+++ main_cnn.py
@@ -8,6 +8,7 @@ import load_data
 # import numpy as np
 #############################    
 import torch
+import sys
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
@@ -18,17 +19,134 @@ import pandas as pd
 from sklearn.metrics import precision_score,f1_score,classification_report as cr
 from sklearn.metrics import confusion_matrix as cm
 
+import spacy
+nlp = spacy.load('en')
 import random
+
+
 import re
+#########################
+import re
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+URL_REGEX = "(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+"
+EMAIL_REGEX = "[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*"
+USER_REGEX = "\\@\\w+"
+
+INVISIBLE_REGEX = '[\x00-\x08\x0b\x0c\x0e-\x1f\x7f-\xff]'
+QUOTATION_REGEX = "[”“❝„\"]+"
+APOSTROPHE_REGEX = "[‘´’̇]+"
+
+PRICE_REGEX = '([\$£€¥][0-9]+|[0-9]+[\$£€¥])'
+DATE_REGEX = '(?:(?:(?:(?:(?<!:)\\b\\\'?\\d{1,4},? ?)?\\b(?:[Jj]an(?:uary)?|[Ff]eb(?:ruary)?|[Mm]ar(?:ch)?|[Aa]pr(?:il)?|May|[Jj]un(?:e)?|[Jj]ul(?:y)?|[Aa]ug(?:ust)?|[Ss]ept?(?:ember)?|[Oo]ct(' \
+             '?:ober)?|[Nn]ov(?:ember)?|[Dd]ec(?:ember)?)\\b(?:(?:,? ?\\\'?)?\\d{1,4}(?:st|nd|rd|n?th)?\\b(?:[,\\/]? ?\\\'?\\d{2,4}[a-zA-Z]*)?(?: ?- ?\\d{2,4}[a-zA-Z]*)?(?!:\\d{1,' \
+             '4})\\b))|(?:(?:(?<!:)\\b\\\'?\\d{1,4},? ?)\\b(?:[Jj]an(?:uary)?|[Ff]eb(?:ruary)?|[Mm]ar(?:ch)?|[Aa]pr(?:il)?|May|[Jj]un(?:e)?|[Jj]ul(?:y)?|[Aa]ug(?:ust)?|[Ss]ept?(?:ember)?|[Oo]ct(' \
+             '?:ober)?|[Nn]ov(?:ember)?|[Dd]ec(?:ember)?)\\b(?:(?:,? ?\\\'?)?\\d{1,4}(?:st|nd|rd|n?th)?\\b(?:[,\\/]? ?\\\'?\\d{2,4}[a-zA-Z]*)?(?: ?- ?\\d{2,4}[a-zA-Z]*)?(?!:\\d{1,4})\\b)?))|(?:\\b(' \
+             '?<!\\d\\.)(?:(?:(?:[0123]?[0-9][\\.\\-\\/])?[0123]?[0-9][\\.\\-\\/][12][0-9]{3})|(?:[0123]?[0-9][\\.\\-\\/][0123]?[0-9][\\.\\-\\/][12]?[0-9]{2,3}))(?!\\.\\d)\\b))'
+TIME_REGEX = '(?:(?:\d+)?\.?\d+(?:AM|PM|am|pm|a\.m\.|p\.m\.))|(?:(?:[0-2]?[0-9]|[2][0-3]):(?:[0-5][0-9])(?::(?:[0-5][0-9]))?(?: ?(?:AM|PM|am|pm|a\.m\.|p\.m\.))?)'
+
+import spacy
+nlp = spacy.load('en')
+def tokenize_en(text):
+  
+  
+  text = re.sub('[\u0370-\u03ff]', '', text)  # Greek and Coptic
+  text = re.sub('[\u0400-\u052f]', '', text)  # Cyrillic and Cyrillic Supplementary
+  text = re.sub('[\u2500-\u257f]', '', text)  # Box Drawing
+  text = re.sub('[\u2e80-\u4dff]', '', text)  # from CJK Radicals Supplement
+  text = re.sub('[\u4e00-\u9fff]', '', text)  # CJK Unified Ideographs
+  text = re.sub('[\ue000-\uf8ff]', '', text)  # Private Use Area
+  text = re.sub('[\uff00-\uffef]', '', text)  # Halfwidth and Fullwidth Forms
+  text = re.sub('[\ufe30-\ufe4f]', '', text)  # CJK Compatibility Forms
+
+  text = re.sub(INVISIBLE_REGEX, '', text)
+  text = re.sub(QUOTATION_REGEX, '\"', text)
+  text = re.sub(APOSTROPHE_REGEX, '\'', text)
+  text = re.sub(r"\s+", " ", text)
+  
+  text = re.sub(PRICE_REGEX, r" <PRICE> ", text)
+  text = re.sub(TIME_REGEX, r" <TIME> ", text)
+  text = re.sub(DATE_REGEX, r" <DATE> ", text)
+
+  text = re.sub(r"([a-zA-Z]+)([0-9]+)", r"\1 \2", text)
+  text = re.sub(r"([0-9]+)([a-zA-Z]+)", r"\1 \2", text)
+  text = re.sub(r" [0-9]+ ", r" <NUMBER> ", text)
+
+  text = re.sub(r"(\b)([Ii]) 'm", r"\1\2 am", text)
+  text = re.sub(r"(\b)([Tt]hey|[Ww]e|[Ww]hat|[Ww]ho|[Yy]ou) 're", r"\1\2 are", text)
+  text = re.sub(r"(\b)([Ll]et) 's", r"\1\2 us", text)
+  text = re.sub(r"(\b)([Hh]e|[Ii]|[Ss]he|[Tt]hey|[Ww]e|[Ww]hat|[Ww]ho|[Yy]ou) 'll", r"\1\2 will", text)
+  text = re.sub(r"(\b)([Ii]|[Ss]hould|[Tt]hey|[Ww]e|[Ww]hat|[Ww]ho|[Ww]ould|[Yy]ou) 've", r"\1\2 have", text)
+  
+  text = re.sub(r"(\b)([Aa]re|[Cc]ould|[Dd]id|[Dd]oes|[Dd]o|[Hh]ad|[Hh]as|[Hh]ave|[Ii]s|[Mm]ight|[Mm]ust|[Ss]hould|[Ww]ere|[Ww]as|[Ww]ould) n't", r"\1\2 not", text)
+  text = re.sub(r"(\b)([Cc]a) n't", r"\1\2n not", text)
+  text = re.sub(r"(\b)([Ww]) on't", r"\1\2ill not", text)
+  text = re.sub(r"(\b)([Ss])han't", r"\1\2hall not", text)
+  text = re.sub(r" n't ", r" not ", text)
+
+  
+  text = re.sub(r"([‼.,;:?!…])+", r" \1 ", text)
+  text = re.sub(r"([()])+", r" \1 ", text)
+  text = re.sub(r"[-]+", r" - ", text)
+  text = re.sub(r"[_]+", r" _ ", text)
+  text = re.sub(r"[=]+", r" = ", text)
+  text = re.sub(r"[\&]+", r" \& ", text)
+  text = re.sub(r"[\+]+", r" \+ ", text)
+
+  text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
+  text = re.sub(r"what's", "what is", text)
+  text = re.sub(r"\'s", "", text)
+  text = re.sub(r"\'ve", "have", text)
+  text = re.sub(r"can't", "cannot", text)
+  text = re.sub(r"n't", "not", text)
+  text = re.sub(r"i'm", "i am", text)
+  text = re.sub(r"\'re", "are", text)
+  text = re.sub(r"\'d", "would", text)
+  text = re.sub(r"\'ll", "will", text)
+  text = re.sub(r",", "", text)
+  text = re.sub(r"\.", "", text)
+  text = re.sub(r"!", "!", text)
+  text = re.sub(r"\/", "", text)
+  text = re.sub(r"\^", "^", text)
+  text = re.sub(r"\+", "+", text)
+  text = re.sub(r"\-", "-", text)
+  text = re.sub(r"\=", "=", text)
+  text = re.sub(r"'", "", text)
+  text = re.sub(r"<", "", text)
+  text = re.sub(r">", "", text)
+  text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
+  text = re.sub(r":", ":", text)
+  text = re.sub(r" e g ", "eg", text)
+  text = re.sub(r" b g ", "bg", text)
+  text = re.sub(r" u s ", "american", text)
+  text = re.sub(r"\0s", "0", text)
+  text = re.sub(r"e - mail", "email", text)
+  text = re.sub(r"j k", "jk", text)
+  text = re.sub(' +', ' ',text)
+  tokenized=[tok.text for tok in nlp(text)]
+  t=[]
+  for tok in tokenized:
+    if tok == " " or tok == "  " or tok == "-":
+      continue
+    else:
+      t.append(tok)
+#   if len(tokenized) < 3:
+#         tokenized += ['<pad>'] * (3 - len(tokenized))
+  return t
+
+
+#########################
 from torch.backends import cudnn
-SEED = 1234
-random.seed(SEED)
-np.random.seed(SEED)
-torch.manual_seed(SEED)
+seed = 1234
+
+# seed=random.randint(0, 10000)
+print(seed)
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
 # seed = 0
 # torch.manual_seed(seed)
 if torch.cuda.is_available():
-  torch.cuda.manual_seed_all(SEED)
+  torch.cuda.manual_seed_all(seed)
 torch.backends.cudnn.enabled = False 
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
@@ -36,8 +154,8 @@ torch.backends.cudnn.benchmark = False
 from models.LSTM import LSTMClassifier
 from models.LSTM_Attn import AttentionModel
 from models.RCNN import RCNN
+from models.CNN import CNN
 from models.selfAttention import SelfAttention
-from CNN1d import CNN1d
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 TEXT, vocab_size, word_embeddings, train_iter, valid_iter, test_iter = load_data.load_dataset()
 
@@ -84,16 +202,22 @@ TEXT, vocab_size, word_embeddings, train_iter, valid_iter, test_iter = load_data
 
 # learning_rate = 2e-5
 batch_size = 64
-INPUT_DIM = len(TEXT.vocab)
-EMBEDDING_DIM = 100
-N_FILTERS = 192
-HIDDEN_DIM=250
-Dropout_2=0.2
-FILTER_SIZES = [2,3,4]
-OUTPUT_DIM = 1
-DROPOUT = 0.5
-# PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
-model = CNN1d(vocab_size,word_embeddings,INPUT_DIM,HIDDEN_DIM,EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM,DROPOUT,Dropout_2,word_embeddings)
+output_size = 1
+hidden_size = 256
+N_LAYERS = 2
+#changed from 0.2 to 0.4
+DROPOUT = 0.2
+embedding_length = 100
+in_channels=1
+out_channels=192
+kernel_heights=[2,3,4,5]
+stride=1
+padding=0
+keep_probab=0.4
+
+
+model = AttentionModel(batch_size, output_size, hidden_size, vocab_size, embedding_length, word_embeddings,N_LAYERS,DROPOUT)
+# model = CNN(batch_size, output_size, in_channels, out_channels, kernel_heights, stride, padding, keep_probab, vocab_size, embedding_length, word_embeddings)
 # loss_fn = F.cross_entropy
 
 import torch.optim as optim
@@ -132,12 +256,14 @@ def train(model, iterator, optimizer, criterion):
 
   for batch in iterator:
       text= batch.text[0]
+      
       target=batch.label
 #       target = torch.autograd.Variable(target).long()
-      target = target.reshape([target.shape[0],1])
+      target=target.reshape([target.shape[0],1])
       optimizer.zero_grad()
 #       print(batch)
       predictions = model(text)
+#       print(predictions.size())
 #       predictions=predictions.reshape([predictions.shape[0]])
       loss = criterion(predictions, target)
 
@@ -230,11 +356,13 @@ def evaluate(model, iterator, criterion):
 
       for batch in iterator:
           text= batch.text[0]
+	  
           predictions = model(text)
 # 	  predictions=predictions.reshape([predictions.shape[0]])
           target=batch.label
-#       target = torch.autograd.Variable(target).long()
-          target = target.reshape([target.shape[0],1])
+	  
+#         target = torch.autograd.Variable(target).long()
+          target=target.reshape([target.shape[0],1])
           loss = criterion(predictions, target)
           
           acc,f1,y_mini,pred_mini = binary_accuracy(predictions, target)
@@ -299,3 +427,42 @@ print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc*100:.2f}%| Test_f1 : {t
 print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc*100:.2f}%| Test_f1_bin : {f1:.4f}')
 print(f'Test Loss: {test_loss:.3f} | Test Acc: {test_acc*100:.2f}%| Test_f1_mac : {f1_macro:.4f}')   
 ###############################################
+
+
+def predict_sentiment(model):
+    model.eval()
+    l=[]
+    df=pd.read_csv("SubtaskA_Trial_Test_Labeled - Copy.csv")
+    with torch.no_grad():
+		
+	    for i in range(len(df)):
+	      tokenized = TEXT.preprocess(df['data'][i])
+
+	      indexed = [TEXT.vocab.stoi[t] for t in tokenized]
+	#       print(len(tokenized))
+	      test_sen = np.asarray(indexed)
+	      test_sen=np.asarray(test_sen)
+	      test_sen = torch.LongTensor(test_sen)
+	      test_tensor = Variable(test_sen, volatile=True)
+	      test_tensor = test_tensor.cuda()
+	      test_tensor=test_tensor.reshape([1,test_tensor.shape[0]])
+	#       length = [len(indexed)]
+	#       tensor = torch.LongTensor(indexed).to(device)
+
+	#       tensor = tensor.unsqueeze(0)
+	#       print(test_tensor.size())
+	#       length_tensor = torch.LongTensor(length)
+	#       test_tensor = Variable(tensor, volatile=True)
+	#       test_tensor = test_tensor.cuda()
+	#       test_tensor=test_tensor.unsqueeze(1)
+	      prediction = torch.sigmoid(model(test_tensor,1))
+	#       print(prediction)
+	      l.append(((prediction[0][0]).data).cpu().numpy())
+
+    df['preds']=l
+    import csv
+    df.to_csv('predidctions.csv')
+    return(l)
+    
+    
+a=predict_sentiment(model)
