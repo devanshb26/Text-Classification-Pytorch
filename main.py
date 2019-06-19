6858bfd81396a0b973a55b29fe5a7cf858aa19e3
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-19 17:21:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -436,34 +436,35 @@ def predict_sentiment(model):
     with torch.no_grad():
 		
 	    for i in range(len(df)):
-	      tokenized = TEXT.preprocess(df['data'][i])
-              if len(tokenized)>=4:
-		
-		indexed = [TEXT.vocab.stoi[t] for t in tokenized]
-	#       print(len(tokenized))
-		print(i)
-		test_sen = np.asarray(indexed)
-		test_sen=np.asarray(test_sen)
-		test_sen = torch.LongTensor(test_sen)
-	# 	      F.pad(test_sen, pad=(0, 40-test_sen.shape[0]), mode='constant', value=0)
-		print(test_sen.size())
-		test_tensor = Variable(test_sen, volatile=True)
-		test_tensor = test_tensor.cuda()
-		test_tensor=test_tensor.reshape([1,test_sen.shape[0]])
-	#       length = [len(indexed)]
-	#       tensor = torch.LongTensor(indexed).to(device)
+		tokenized = TEXT.preprocess(df['data'][i])
+	      
+              
+		if len(tokenized)>=4:
+			indexed = [TEXT.vocab.stoi[t] for t in tokenized]
+		#       print(len(tokenized))
+			print(i)
+			test_sen = np.asarray(indexed)
+			test_sen=np.asarray(test_sen)
+			test_sen = torch.LongTensor(test_sen)
+		# 	      F.pad(test_sen, pad=(0, 40-test_sen.shape[0]), mode='constant', value=0)
+			print(test_sen.size())
+			test_tensor = Variable(test_sen, volatile=True)
+			test_tensor = test_tensor.cuda()
+			test_tensor=test_tensor.reshape([1,test_sen.shape[0]])
+		#       length = [len(indexed)]
+		#       tensor = torch.LongTensor(indexed).to(device)
 
-	#       tensor = tensor.unsqueeze(0)
-	#       print(test_tensor.size())
-	#       length_tensor = torch.LongTensor(length)
-	#       test_tensor = Variable(tensor, volatile=True)
-	#       test_tensor = test_tensor.cuda()
-	#       test_tensor=test_tensor.unsqueeze(1)
-		prediction = torch.sigmoid(model(test_tensor,1))
-	#       print(prediction)
-		l.append(((prediction[0][0]).data).cpu().numpy())
-	      else:
-		l.append('**')
+		#       tensor = tensor.unsqueeze(0)
+		#       print(test_tensor.size())
+		#       length_tensor = torch.LongTensor(length)
+		#       test_tensor = Variable(tensor, volatile=True)
+		#       test_tensor = test_tensor.cuda()
+		#       test_tensor=test_tensor.unsqueeze(1)
+			prediction = torch.sigmoid(model(test_tensor,1))
+		#       print(prediction)
+			l.append(((prediction[0][0]).data).cpu().numpy())
+	        else:
+		        l.append('**')
 		
 
     df['preds']=l
