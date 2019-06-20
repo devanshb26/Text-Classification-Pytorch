837274ac04e09c72e02f800cccb8bd56f797b0cd
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-20 14:11:58 +0530

    Update main_cnn.py

--- main_cnn.py
+++ main_cnn.py
@@ -205,7 +205,7 @@ TEXT, vocab_size, word_embeddings, train_iter, valid_iter, test_iter = load_data
 batch_size = 64
 output_size = 1
 hidden_size = 256
-N_LAYERS = 1
+N_LAYERS = 2
 #changed from 0.2 to 0.4
 DROPOUT = 0.2
 embedding_length = 100
