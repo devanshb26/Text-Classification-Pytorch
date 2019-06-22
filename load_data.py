4ce1d4a2b574f008bfff39a1335fe238252490c8
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-22 19:00:42 +0530

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -103,7 +103,7 @@ def tokenize_en(text):
   text = re.sub(r"\0s", "0", text)
   text = re.sub(r"e - mail", "email", text)
   text = re.sub(r"j k", "jk", text)
-#   text = re.sub(' +', ' ',text)
+  text = re.sub(' +', ' ',text)
   tokenized=[tok.text for tok in nlp(text)]
   t=[]
   for tok in tokenized:
