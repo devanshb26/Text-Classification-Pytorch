a2d1cc837a2d2cdcce76a1da039bbbd2aa8f37b5
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-22 11:39:21 +0530

    Update CNN_LSTM_attn.py

--- models/CNN_LSTM_attn.py
+++ models/CNN_LSTM_attn.py
@@ -47,31 +47,7 @@ class CNN_LSTM(nn.Module):
 		self.dropout_embd = nn.Dropout(0.5)
 		
 	def attention_net(self,lstm_output,final_state):
-		
-                """ 
-		Now we will incorporate Attention mechanism in our LSTM model. In this new model, we will use attention to compute soft alignment score corresponding
-		between each of the hidden_state and the last hidden_state of the LSTM. We will be using torch.bmm for the batch matrix multiplication.
-		
-		Arguments
-		---------
-		
-		lstm_output : Final output of the LSTM which contains hidden layer outputs for each sequence.
-		final_state : Final time-step hidden state (h_n) of the LSTM
-		
-		---------
-		
-		Returns : It performs attention mechanism by first computing weights for each of the sequence present in lstm_output and and then finally computing the
-				  new hidden state.
-				  
-		Tensor Size :
-					hidden.size() = (batch_size, hidden_size)
-					attn_weights.size() = (batch_size, num_seq)
-					soft_attn_weights.size() = (batch_size, num_seq)
-					new_hidden_state.size() = (batch_size, hidden_size)
-					  
-		"""
 		hidden = final_state.squeeze(0)
-# 		hidden = final_state.squeeze(0)
 		attn_weights = torch.bmm(lstm_output, hidden.unsqueeze(2)).squeeze(2)
 		soft_attn_weights = F.softmax(attn_weights, 1)
 		new_hidden_state = torch.bmm(lstm_output.transpose(1, 2), soft_attn_weights.unsqueeze(2)).squeeze(2)
