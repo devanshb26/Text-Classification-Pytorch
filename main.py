cad2d606c470fa8b6768dc3ce900839ded29b9db
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2019-06-22 16:32:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -202,12 +202,12 @@ TEXT, vocab_size, word_embeddings, train_iter, valid_iter, test_iter = load_data
 ########################################
 
 # learning_rate = 2e-5
-batch_size = 64
+batch_size = 32
 output_size = 1
 hidden_size = 256
 N_LAYERS = 1
 #changed from 0.2 to 0.4
-DROPOUT = 0.3
+DROPOUT = 0.2
 embedding_length = 100
 in_channels=1
 out_channels=192
